7d8409bfdc203b39f37ea0dabcc6edb2cf2e7cb9
Jesse Lucas <jessejlucas@hotmail.com>
2022-10-02 18:26:29 +0800

    fixed deployment issue

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import app from "./create-server";
 import path from "path";
 
-const [host, port] = ["localhost", 3000];
+const [host, port] = ["http://localhost", 3000 || process.env.PORT];
 
 app.set("views", path.join(__dirname, "./website/views"));
 app.set("view engine", "pug");
